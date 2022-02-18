3207556133ce0497bc547062a14fa62ba28dcd12
Quentin Stievenard <qstievenard@gmail.com>
2022-02-18 10:13:43 +0100

    :sparkles: for search

--- search.js
+++ search.js
@@ -1,16 +1,9 @@
-/**
- * SEARCH ALGORITHM 1
- * Here is all the logic to process a user request and return a list of corresponding recipes
- * This first algorithm has not pre-treatment of the data, the array data go throught too much
- * "for of" loops who'll only keep the corresponding recipes.
- */
-
 // Import database with all the recipes
-import data from "./assets/data/data.json"
+import data from "./assets/data/data.json";
 // store last results
 let lastSearch = [];
 export function getLastSearch() {
-    return lastSearch;
+  return lastSearch;
 }
 
 /**
@@ -21,16 +14,16 @@ export function getLastSearch() {
  * @returns {array} - array of objects (corresponding recipes)
  */
 export function search(request, appliance, ustensil, ingredients) {
-    console.time("search");
-    let recipes = data.recipes;
-    recipes = matchAppliance(recipes, appliance.toLowerCase());
-    recipes = matchUstensils(recipes, ustensil.toLowerCase());
-    recipes = matchIngredients(recipes, ingredients);
-    recipes = matchContent(recipes, request.toLowerCase());
-    //save the result
-    lastSearch = [appliance, ustensil, ingredients, recipes];
-    console.timeEnd("search");
-    return recipes;
+  console.time("search");
+  let recipes = data.recipes;
+  recipes = matchAppliance(recipes, appliance.toLowerCase());
+  recipes = matchUstensils(recipes, ustensil.toLowerCase());
+  recipes = matchIngredients(recipes, ingredients);
+  recipes = matchContent(recipes, request.toLowerCase());
+  //save the result
+  lastSearch = [appliance, ustensil, ingredients, recipes];
+  console.timeEnd("search");
+  return recipes;
 }
 
 /**
@@ -40,13 +33,13 @@ export function search(request, appliance, ustensil, ingredients) {
  * @returns {array} - array of objects (corresponding recipes)
  */
 function matchAppliance(recipes, appliance) {
-    let recipesMatched = [];
-    for (let recipe of recipes) {
-        if (recipe.appliance.toLowerCase().includes(appliance)) {
-            recipesMatched.push(recipe)
-        }
+  let recipesMatched = [];
+  for (let recipe of recipes) {
+    if (recipe.appliance.toLowerCase().includes(appliance)) {
+      recipesMatched.push(recipe);
     }
-    return recipesMatched;
+  }
+  return recipesMatched;
 }
 
 /**
@@ -56,13 +49,16 @@ function matchAppliance(recipes, appliance) {
  * @returns {array} - array of objects (corresponding recipes)
  */
 function matchUstensils(recipes, ustensil) {
-    let recipesMatched = [];
-    for (let recipe of recipes) {
-        if (ustensil == "" || recipe.ustensils.filter(usten => usten.includes(ustensil)).length > 0) {
-            recipesMatched.push(recipe)
-        }
+  let recipesMatched = [];
+  for (let recipe of recipes) {
+    if (
+      ustensil == "" ||
+      recipe.ustensils.filter((usten) => usten.includes(ustensil)).length > 0
+    ) {
+      recipesMatched.push(recipe);
     }
-    return recipesMatched;
+  }
+  return recipesMatched;
 }
 
 /**
@@ -71,21 +67,24 @@ function matchUstensils(recipes, ustensil) {
  * @param {array} ingredients
  * @returns {array} - array of objects (corresponding recipes)
  */
- function matchIngredients(recipes, ingredients) {
-    let recipesMatched = [];
-    for (let recipe of recipes) {
-        let ingredientsMatch = []
-        ingredients.forEach(ingredient => {
-            ingredientsMatch.push(
-                recipe.ingredients.filter(recIngredient =>
-                    recIngredient.ingredient.toLowerCase().includes(ingredient.toLowerCase())    
-                ).length > 0 
-        )})
-        if (ingredientsMatch.every(match => match == true)) {
-            recipesMatched.push(recipe)
-        }
+function matchIngredients(recipes, ingredients) {
+  let recipesMatched = [];
+  for (let recipe of recipes) {
+    let ingredientsMatch = [];
+    ingredients.forEach((ingredient) => {
+      ingredientsMatch.push(
+        recipe.ingredients.filter((recIngredient) =>
+          recIngredient.ingredient
+            .toLowerCase()
+            .includes(ingredient.toLowerCase())
+        ).length > 0
+      );
+    });
+    if (ingredientsMatch.every((match) => match == true)) {
+      recipesMatched.push(recipe);
     }
-    return recipesMatched;
+  }
+  return recipesMatched;
 }
 
 /**
@@ -95,15 +94,17 @@ function matchUstensils(recipes, ustensil) {
  * @returns {array} - array of objects (corresponding recipes)
  */
 function matchContent(recipes, request) {
-    let recipesMatched = [];
-    for (let recipe of recipes) {
-        if (recipe.name.toLowerCase().includes(request) 
-            || recipe.description.toLowerCase().includes(request)
-            || recipe.ingredients
-                .filter(ingredient => ingredient.ingredient
-                    .toLowerCase().includes(request)).length > 0) {
-            recipesMatched.push(recipe)
-        }
+  let recipesMatched = [];
+  for (let recipe of recipes) {
+    if (
+      recipe.name.toLowerCase().includes(request) ||
+      recipe.description.toLowerCase().includes(request) ||
+      recipe.ingredients.filter((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(request)
+      ).length > 0
+    ) {
+      recipesMatched.push(recipe);
     }
-    return recipesMatched;
+  }
+  return recipesMatched;
 }
